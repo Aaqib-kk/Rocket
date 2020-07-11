3c6d487e2dcc5ec2f3bec6cc3ca185c6a16c4006
aaqib-kk <aqib.khan.25@iiee.edu.pk>
2020-07-11 21:31:50 +0500

    Second commit

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+Rocket is web frame work for rust. to write fast and secure web applications 
+it is type safe, dont have to deal with strings 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/hello")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+#[get("/hello/<name>")]
+fn world_o(name: String) -> String {
+    format!("Hello: {}!", name)
+}
+#[get("/hello/<num>")]
+fn world_n(num: usize) -> String {
+    format!("The Total Number is: {}!", num+50)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+    .mount("/", routes![world, world_n])
+    .launch();
 }
